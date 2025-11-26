45a9d505b150e26d68c6915e9e9e7cf77fa71a7d
Vipin Goriparthi <73373574+vipiin@users.noreply.github.com>
2025-11-26 22:02:33 +0530

    Add hash location strategy to router configuration

--- angular-frontend/src/app/app.config.ts
+++ angular-frontend/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter,withHashLocation} from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
@@ -8,7 +8,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes,withHashLocation(),
     provideHttpClient()
   ]
 };
