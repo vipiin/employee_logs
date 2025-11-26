8d68a5455606f81dd25d13beb8df7023a1d515ed
Vipin Goriparthi <73373574+vipiin@users.noreply.github.com>
2025-11-26 22:03:36 +0530

    Fix syntax error in app.config.ts

--- angular-frontend/src/app/app.config.ts
+++ angular-frontend/src/app/app.config.ts
@@ -8,7 +8,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes,withHashLocation(),
+    provideRouter(routes,withHashLocation()),
     provideHttpClient()
   ]
 };
